89e86a7763570761ce296b9b5dfe8f00b9d6d7a8
rodrigopinesso <142932782+rodrigopinesso@users.noreply.github.com>
2023-08-24 20:22:40 -0300

    Add files via upload

---
+++ exercicioteste/src/main/java/org/example/TipoTriangulo.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class TipoTriangulo {
+    public static String identificarTipo(double lado1, double lado2,
+                                         double lado3) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
+    }
+}
---
+++ exercicioteste/src/test/java/TrianguloTest.java
@@ -0,0 +1,2 @@
+public class TrianguloTest {
+}
